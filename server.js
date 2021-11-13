73a55154b5878297ad48b5a4b2b1a79877f2cb11
Michael Zhang <mzhang0213@gmail.com>
2021-11-13 16:27:40 -0500

    get root

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || "12232";
 
+app.use(express.static('public'));
+
 app.listen(PORT, ()=>{
 	console.log("listening asdfsdf " + PORT)
 })
