d8ab89331263c01d495c30da2925274a56fb75a4
Michael Zhang <mzhang0213@gmail.com>
2021-11-13 16:21:43 -0500

    no reqres

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT | 12232;
+const PORT = process.env.PORT || "12232";
 
 app.listen(PORT, ()=>{
 	console.log("listening asdfsdf " + PORT)
