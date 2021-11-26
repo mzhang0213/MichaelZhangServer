936712185566178f63ce44752c12cbe617c2fe65
Michael Zhang <mzhang0213@gmail.com>
2021-11-25 21:41:15 -0500

    routing for uploads

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || "12232";
 
 app.use(express.static('public'));
 
+app.get("/uploads/*", (req, res)=>{
+    res.sendFile(__dirname+req.url.substring(req.url.indexOf("/uploads/")));
+});
+
 app.listen(PORT, ()=>{
 	console.log("listening asdfsdf " + PORT)
 })
