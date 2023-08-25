73cc440797b922a458d4e5d0713b0e64ff360c37
Michael Zhang <mzhang0213@gmail.com>
2023-08-24 22:21:47 -0400

    annother attempt

--- public/hippohack/service.js
+++ public/hippohack/service.js
@@ -43,24 +43,10 @@ self.addEventListener('activate', async (event) => {
     const options = { applicationServerKey, userVisibleOnly: true }
     const subscription = await self.registration.pushManager.subscribe(options);
     
-    event.waitUntil(
-      (async () => {
-        // Exit early if we don't have access to the client.
-        // Eg, if it's cross-origin.
-        if (!event.clientId) return;
-  
-        // Get the client.
-        const client = await clients.get(event.clientId);
-        // Exit early if we don't get the client.
-        // Eg, if it closed.
-        if (!client) return;
-  
-        // Send a message to the client.
-        client.postMessage({
-          msg: "Hey I just got a fetch from you!"
-        });
-      })(),
-    );
+    const client = await clients.get(event.clientId);
+    client.postMessage({
+      msg: "Hey I just got a fetch from you!"
+    });
 
     const response = await saveSubscription(subscription)
     console.log(response)
