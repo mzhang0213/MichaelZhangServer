043c4a7bc5e243bee83872b62be99607139d9b23
Michael Zhang <mzhang0213@gmail.com>
2023-08-25 11:59:57 -0400

    i hate myself

--- server.js
+++ server.js
@@ -233,11 +233,10 @@ app.post("/hh-anno", async(req,res)=>{
 app.post("/hh-save-sub",async(req,res)=>{
 	try{
 		//data: req.body.user req.body.sub
-		console.log("started")
 		await client.connect();
 		const db = client.db("hippohack2023").collection("subs");
+		const currContent = await db.findOne();
 		var db_subs = currContent.subs;
-		console.log(db_subs)
 		var submit = [];
 		for (var i=0;i<db_subs.length;i++){
 			submit.push(db_subs[i]);
