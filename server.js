70608985bd2e60bba536f08077e8f84ce43b7b20
Michael Zhang <mzhang0213@gmail.com>
2023-08-27 21:38:16 -0400

    testing

--- server.js
+++ server.js
@@ -431,6 +431,7 @@ app.post("/hh-getMembers",async (req,res)=>{
 		const db = client.db("hippohack2023").collection("accounts");
 		const currContent = await db.findOne();
 		var db_group = currContent.groups;
+		console.log(currContent);
 		var msg = {
 			members:db_group.members
 		}
