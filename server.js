dd2bb01bab524edfb0bdac655540ded1c3129b02
Michael Zhang <mzhang0213@gmail.com>
2023-08-28 14:45:06 -0400

    bug fix

--- server.js
+++ server.js
@@ -191,6 +191,7 @@ app.post("/hh-glogin", async (req,res)=>{
 				}
 			}
 			await db.updateOne(filter,updateDoc);
+			
 		}else {
 			//new group, but i want to send confirmation that they are creating new group
 			msg.error=1;
