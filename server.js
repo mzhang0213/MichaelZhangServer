3d1a8ea2863541eea55a4e793cac5e2df517c16f
Michael Zhang <mzhang0213@gmail.com>
2024-01-22 21:59:52 -0500

    spotify playlist import support

--- server.js
+++ server.js
@@ -1582,6 +1582,109 @@ request.post(authOptions, function(error, response, body) {
 });
 });
 
+
+app.get('/musicQuiz/login', function(req, res) {
+	var shortUrl = req.url.substring(req.url.indexOf("?")+1);
+	lastPlaylist=querystring.parse(shortUrl).playlistInfo;
+	var state = generateRandomString(16);
+	res.cookie(stateKey, state);
+
+	// your application requests authorization
+	var scope = 'playlist-read-private';
+	res.redirect('https://accounts.spotify.com/authorize?' +
+		querystring.stringify({
+			response_type: 'code',
+			client_id: client_id,
+			scope: scope,
+			redirect_uri: redirect_uri,
+			state: state
+		})
+	);
+});
+
+app.get('/musicQuiz/callback', function(req, res) {
+
+// your application requests refresh and access tokens
+// after checking the state parameter
+
+var code = req.query.code || null;
+var state = req.query.state || null;
+var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+if (state === null || state !== storedState) {
+	res.redirect('/musicQuiz/#' +
+	querystring.stringify({
+		error: 'state_mismatch'
+	}));
+} else {
+	res.clearCookie(stateKey);
+	var authOptions = {
+		url: 'https://accounts.spotify.com/api/token',
+		form: {
+			code: code,
+			redirect_uri: redirect_uri,
+			grant_type: 'authorization_code'
+		},
+		headers: {
+			'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+		},
+		json: true
+	};
+
+	request.post(authOptions, function(error, response, body) {
+	if (!error && response.statusCode === 200) {
+
+		var access_token = body.access_token,
+			refresh_token = body.refresh_token;
+
+		res.redirect('/musicQuiz/app.html?' +
+
+		//  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+		//HERE IS WHERE CHANGE THE APP REDIR, CHANGE THIS IF WANT TO CHANGE THE SITE NAME LATA
+		//HERE IS WHERE CHANGE THE APP REDIR, CHANGE THIS IF WANT TO CHANGE THE SITE NAME LATA
+		//HERE IS WHERE CHANGE THE APP REDIR, CHANGE THIS IF WANT TO CHANGE THE SITE NAME LATA
+		//HERE IS WHERE CHANGE THE APP REDIR, CHANGE THIS IF WANT TO CHANGE THE SITE NAME LATA
+
+		querystring.stringify({
+			access_token: access_token,
+			refresh_token: refresh_token,
+			playlistInfo: lastPlaylist
+		}));
+	} else {
+		res.redirect('/musicQuiz/?' +
+		querystring.stringify({
+			error: 'invalid_token'
+		}));
+	}
+	});
+}
+});
+
+app.get('/musicQuiz/refresh_token', function(req, res) {
+
+// requesting access token from refresh token
+var refresh_token = req.query.refresh_token;
+var authOptions = {
+	url: 'https://accounts.spotify.com/api/token',
+	headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+	form: {
+	grant_type: 'refresh_token',
+	refresh_token: refresh_token
+	},
+	json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+	if (!error && response.statusCode === 200) {
+	var access_token = body.access_token;
+	res.send({
+		'access_token': access_token
+	});
+	}
+});
+});
+
+
 app.get("/getDecks", (req, res)=>{
     var json = {
         files:""
