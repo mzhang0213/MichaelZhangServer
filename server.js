e44185bdfa8ff5a36220f80936b4414ee7d59bb8
Michael Zhang <mzhang0213@gmail.com>
2025-03-12 14:27:44 +0000

    init chgs

--- server.js
+++ server.js
@@ -2743,7 +2743,7 @@ app.get('/spotifyYt/login', function(req, res) {
 			response_type: 'code',
 			client_id: client_id,
 			scope: scope,
-			redirect_uri: 'https://michaelzhangwebsite.herokuapp.com/spotifyYt/callback',
+			redirect_uri: window.location.origin+'/spotifyYt/callback',
 			state: state
 		})
 	);
@@ -2769,7 +2769,7 @@ if (state === null || state !== storedState) {
 		url: 'https://accounts.spotify.com/api/token',
 		form: {
 			code: code,
-			redirect_uri: 'https://michaelzhangwebsite.herokuapp.com/spotifyYt/callback',
+			redirect_uri: window.location.origin+'/spotifyYt/callback',
 			grant_type: 'authorization_code'
 		},
 		headers: {
@@ -2838,7 +2838,7 @@ app.get('/musicQuiz/login', function(req, res) {
 			response_type: 'code',
 			client_id: client_id,
 			scope: scope,
-			redirect_uri: 'https://michaelzhangwebsite.herokuapp.com/musicQuiz/callback',
+			redirect_uri: window.location.origin+'/musicQuiz/callback',
 			state: state
 		})
 	);
@@ -2864,7 +2864,7 @@ if (state === null || state !== storedState) {
 		url: 'https://accounts.spotify.com/api/token',
 		form: {
 			code: code,
-			redirect_uri: 'https://michaelzhangwebsite.herokuapp.com/musicQuiz/callback',
+			redirect_uri: window.location.origin+'/musicQuiz/callback',
 			grant_type: 'authorization_code'
 		},
 		headers: {
@@ -2956,7 +2956,7 @@ var oAuth2Client;
 const SCOPES = ["https://www.googleapis.com/auth/classroom.courses.readonly", "https://www.googleapis.com/auth/classroom.coursework.me.readonly", "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly", "https://www.googleapis.com/auth/classroom.announcements.readonly"];
 
 app.get("/classroom/login", (req,res)=>{
-	const {client_secret, client_id, redirect_uris} = {"client_id":"964270111872-332f6vopavq4lr71hl2ifvel1fh6jpm2.apps.googleusercontent.com","project_id":"michaeltest-1","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["https://michaelzhangwebsite.herokuapp.com/classroom/callback/"]};
+	const {client_secret, client_id, redirect_uris} = {"client_id":"964270111872-332f6vopavq4lr71hl2ifvel1fh6jpm2.apps.googleusercontent.com","project_id":"michaeltest-1","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":[window.location.origin+"/classroom/callback/"]};
 	oAuth2Client = new google.auth.OAuth2(
 		client_id, client_secret, redirect_uris[0]);
 
