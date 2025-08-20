910c3b9d74b992b72d1060f3d29218b3e5e20690
mzhang0213 <85912706+mzhang0213@users.noreply.github.com>
2025-08-20 17:26:17 -0400

    asdf

--- postgres.sql
+++ postgres.sql
@@ -1,17 +1,37 @@
 CREATE TABLE bonsai (
-	username VARCHAR(50),
-	nickname VARCHAR(50),
+username VARCHAR(50),
+nickname VARCHAR(50),
+meal_plan meal[],
+workout_plan workout[],
+
+)
+
+CREATE TYPE plan AS ENUM (
+'meal',
+'workout'
+)
+
+#ENSURE ALL OF THE LISTS HAVE PROPER INDEXING THATS CONSTANT
+CREATE TYPE meal_plan (
+foods food[],
+start DATE,
+end DAT
+) INHERITS (plan)
+
+CREATE TYPE workout_plan (
+workout workout[],
+start DATE,
+end DAT
+) INHERITS (plan)
+
+CREATE TYPE workout (
 
 )
 
 CREATE TYPE food (
-	quantity integer,
-	
+quantity integer,
+nutrients macros
 )
 
-CREATE TYPE nutrient (
-	calories integer,
-	carbs integer,
-	protein integer,
-	fat integer
-)
+CREATE TYPE macros (
+calories inte
