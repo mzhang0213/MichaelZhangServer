a5f9b0e97b3d4eb001db0d88bf91fc545944595b
mzhang0213 <85912706+mzhang0213@users.noreply.github.com>
2025-08-20 17:13:14 -0400

    a

---
+++ postgres.sql
@@ -0,0 +1,17 @@
+CREATE TABLE bonsai (
+	username VARCHAR(50),
+	nickname VARCHAR(50),
+
+)
+
+CREATE TYPE food (
+	quantity integer,
+	
+)
+
+CREATE TYPE nutrient (
+	calories integer,
+	carbs integer,
+	protein integer,
+	fat integer
+)
