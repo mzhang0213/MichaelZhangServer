35664cf26721831298f0fca0e897a5b26bf20bec
Michael Zhang <zhang.michael1@northeastern.edu>
2026-03-17 11:11:07 -0400

    big

--- app/resources/keys.ts
+++ app/resources/keys.ts
@@ -1,3 +1,3 @@
 export const stateKey = 'spotify_auth_state';
-export const client_id = process.env.SPOTIFY_CLIENT_ID;
-export const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+export const client_id = process.env.SPOTIFY_CLIENT_ID as string;
+export const client_secret = process.env.SPOTIFY_CLIENT_SECRET as string;
